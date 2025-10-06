1760e22d746d459129130b507b1f9a4ac5569281
Davi Curty <davicurty850@gmail.com>
2025-10-06 20:27:44 -0300

    feat: starting the server of DG Server

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,24 @@
-import fastify from "fastify";
+import Fastify from "fastify";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const fastify = Fastify({
+  logger: true,
+});
+
+const start = async () => {
+  try {
+    const port = Number(process.env.PORT) || 5000;
+    await fastify.listen({
+      port: port,
+      host: "0.0.0.0",
+    });
+    console.log(`Server Running at http://localhost:${port}`);
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+
+start();
